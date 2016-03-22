dd072760078ec7d8729ec349278b1310d0860526
eromaoda <errocha@openmailbox.org>
2016-03-22 18:53:41 -0300

    ta foda

--- des.c
+++ des.c
@@ -1,12 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+//Declaracao das tabelas de permutacao
+//...
+
+//nao sei o q colocar nas tabelas ! poe qqr valor, ou usa os do slide ?
+
+void permutar();
+void split();
+void combinar();
+char gera_chave(); //qual o retorno ?
+void expansao(); //so deus sabe oq essa funcao de expansao realmente faz pqp
 
 int main(){
 	char msg[8];
-	int op;
+	int i;
 	
 	printf("Informe a mensagem desejada:\n");
 	scanf("%s", msg);
+	
+	
 	
 	return 0;
 }
